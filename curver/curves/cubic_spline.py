52335edd49f9c74bfb93dd5b8898f79ce75947d7
wiatrak2 <wpratkowiecki@gmail.com>
2020-05-07 21:58:00 +0200

    Cubic splines WIP

--- curver/curves/cubic_spline.py
+++ curver/curves/cubic_spline.py
@@ -31,47 +31,49 @@ class CubicSpline(Lagrange):
 
         # allocate buffer matrices
         Li = np.empty(size)
-        Li_1 = np.empty(size-1)
+        Li_1 = np.empty(size - 1)
         z = np.empty(size)
 
         # fill diagonals Li and Li-1 and solve [L][y] = [B]
-        Li[0] = np.sqrt(2*xdiff[0])
+        Li[0] = np.sqrt(2 * xdiff[0])
         Li_1[0] = 0.0
-        B0 = 0.0 # natural boundary
+        B0 = 0.0  # natural boundary
         z[0] = B0 / Li[0]
 
-        for i in range(1, size-1, 1):
-            Li_1[i] = xdiff[i-1] / Li[i-1]
-            Li[i] = np.sqrt(2*(xdiff[i-1]+xdiff[i]) - Li_1[i-1] * Li_1[i-1])
-            Bi = 6*(ydiff[i]/xdiff[i] - ydiff[i-1]/xdiff[i-1])
-            z[i] = (Bi - Li_1[i-1]*z[i-1])/Li[i]
+        for i in range(1, size - 1, 1):
+            Li_1[i] = xdiff[i - 1] / Li[i - 1]
+            Li[i] = np.sqrt(2 * (xdiff[i - 1] + xdiff[i]) - Li_1[i - 1] * Li_1[i - 1])
+            Bi = 6 * (ydiff[i] / xdiff[i] - ydiff[i - 1] / xdiff[i - 1])
+            z[i] = (Bi - Li_1[i - 1] * z[i - 1]) / Li[i]
 
         i = size - 1
-        Li_1[i-1] = xdiff[-1] / Li[i-1]
-        Li[i] = np.sqrt(2*xdiff[-1] - Li_1[i-1] * Li_1[i-1])
-        Bi = 0.0 # natural boundary
-        z[i] = (Bi - Li_1[i-1]*z[i-1])/Li[i]
+        Li_1[i - 1] = xdiff[-1] / Li[i - 1]
+        Li[i] = np.sqrt(2 * xdiff[-1] - Li_1[i - 1] * Li_1[i - 1])
+        Bi = 0.0  # natural boundary
+        z[i] = (Bi - Li_1[i - 1] * z[i - 1]) / Li[i]
 
         # solve [L.T][x] = [y]
-        i = size-1
+        i = size - 1
         z[i] = z[i] / Li[i]
-        for i in range(size-2, -1, -1):
-            z[i] = (z[i] - Li_1[i-1]*z[i+1])/Li[i]
+        for i in range(size - 2, -1, -1):
+            z[i] = (z[i] - Li_1[i - 1] * z[i + 1]) / Li[i]
 
         # find index
         index = xs.searchsorted(t)
-        np.clip(index, 1, size-1, index)
+        np.clip(index, 1, size - 1, index)
 
-        xi1, xi0 = xs[index], xs[index-1]
-        yi1, yi0 = ys[index], ys[index-1]
-        zi1, zi0 = z[index], z[index-1]
+        xi1, xi0 = xs[index], xs[index - 1]
+        yi1, yi0 = ys[index], ys[index - 1]
+        zi1, zi0 = z[index], z[index - 1]
         hi1 = xi1 - xi0
 
         # calculate cubic
-        f_t = zi0/(6*hi1)*(xi1-t)**3 + \
-            zi1/(6*hi1)*(t-xi0)**3 + \
-            (yi1/hi1 - zi1*hi1/6)*(t-xi0) + \
-            (yi0/hi1 - zi0*hi1/6)*(xi1-t)
+        f_t = (
+            zi0 / (6 * hi1) * (xi1 - t) ** 3
+            + zi1 / (6 * hi1) * (t - xi0) ** 3
+            + (yi1 / hi1 - zi1 * hi1 / 6) * (t - xi0)
+            + (yi0 / hi1 - zi0 * hi1 / 6) * (xi1 - t)
+        )
 
         return [widgets.Point(QtCore.QPointF(t[i], f_t[i])) for i in range(len(t))]
 
